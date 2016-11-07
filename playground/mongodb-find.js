a555c8b4ce5b435ea1e41763da1a5183bb5fb4d9
Hal DeLage <hal.delage@teamaol.com>
2016-11-07 10:08:06 -0500

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server.');
+
+    // db.collection('Todos').find({
+    //         _id: new ObjectID('581cd92bfbb4e73a7567efaf')
+    //     }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos!');
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos!');
+    // });
+
+    db.collection('Users').find({name: 'Hal'}).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos!');
+    });
+
+    //db.close();
+});
